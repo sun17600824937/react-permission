e1816e9949b35b215dea8af29d30fdb61e3799d2
sun <sun17600824937@163.com>
2022-03-22 16:45:47 +0800

    mac

--- src/page/Error/index.tsx
+++ src/page/Error/index.tsx
@@ -2,11 +2,11 @@ import { Result } from 'antd';
 
 const Error = () => {
    return (
-   <Result
-    status="404"
-    title="404"
-    subTitle="Sorry, the page you visited does not exist."
-     />
+        <Result
+            status="404"
+            title="404"
+            subTitle="Sorry, the page you visited does not exist."
+        />
  )
 }
 export default Error
--- src/page/Login/index.tsx
+++ src/page/Login/index.tsx
@@ -8,6 +8,5 @@
                     </div>
             )
 
-
     }
     export default Login
